3b1a3e9b9ee15f50baafa652d33ced4cd6d30585
dumbleedore <lucas.pari@icloud.com>
2022-11-22 18:31:21 -0300

    test movieAPI tested

--- src/__tests__/components/search.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render, fireEvent } from "@testing-library/react";
-import { Search } from "../../components/search/search";
-describe("search.tsx", () => {
-  it("should render search component", () => {
-    const { container } = render(<Search />);
-    expect(container).toBeInTheDocument();
-  });
-});
--- src/__tests__/pages/home.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { Home } from "../../pages/home/home";
-import { render } from "@testing-library/react";
-describe("home.tsx", () => {
-  it("should render Home.tsx", () => {
-    const { container } = render(<Home />);
-    expect(container).toBeInTheDocument();
-  });
-});
